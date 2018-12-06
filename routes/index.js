dabe257ca12445c3187a7ec046c6d72caaa4ba8b
Yongdeok An <an-yongdeok-tev@tdi.co.jp>
2018-12-06 16:58:12 +0900

    feature/addUser code Review

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 이 addPage method 도 user.js 로 옮기는게 좋을 듯(user에 관련된 메소드기 때문에)
+// url은 /user/new 가 괜찮지 않을까요?
 router.get('/addPage', function(req,res){
   res.render('../views/users/addUser.ejs')
 })
--- routes/user.js
+++ routes/user.js
@@ -11,12 +11,19 @@ router.get('/', function (req, res, next) {
   })
 })
 
+// url 은 /user 로 변경하는 편이 좋을 것 같습니다.
+// 위에 메소드는 get /user
+// 아래 메소드는 post /user
+// http://sourceflower.tistory.com/17 참조
 router.post('/addUser', function(req,res,next){
+// function ( req, res, next ) { 이런 식으로 띠어쓰기는 같이 맞추는 편이 좋습니다.
   const params ={
+// const params = {
     name: req.param('name'),
     age: req.param('age'),
     address: req.param('address')
   }
+  // 여기도 띄어쓰기
   user.addUser(params,(err,response) =>{
     if(err) console.log(err)
     console.log(response)
